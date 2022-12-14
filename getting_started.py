74ae89c219e6bb5ab89c49f0272681b12a304b72
Febe <alu0101430973@alu.edu.es>
2022-12-14 20:48:24 +0000

    paint mode completo

--- getting_started.py
+++ getting_started.py
@@ -1,9 +1,13 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+imgCanvas = np.zeros((480,640,3), np.uint8) #Canvas para pintar
+paintMode = True #Modo pintar
 
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
@@ -73,17 +77,22 @@ with mp_hands.Hands(
       # cv2.rectangle(image, start_point (top-left), end_point (bottom-right), color, thickness)
       #cv2.putText(image, str(sum(fingersUp)), (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
       
+      #Contador de dedos levantados
       for i in range(4):
         if lm[tipIds[i+1]].y - lm[tipIds[i+1]-1].y < 0:
           fingersUp[i+1] = 1
         else: 
           fingersUp[i+1] = 0
 
-      thumbRight = False
-      konShow = False
+      #Flags
+      thumbRight = False #El pulgar está en la derecha
+      konShow = False #Kon está mostrado
+
+      #Pulgar a la derecha
       if lm[tipIds[0]].x > lm[tipIds[4]].x:
         thumbRight = True
 
+      #Pulgar arriba
       if lm[tipIds[0]].x - lm[tipIds[0]-1].x < 0 and not thumbRight:
         fingersUp[0] = 1
       elif lm[tipIds[0]].x - lm[tipIds[0]-1].x > 0 and thumbRight:
@@ -91,20 +100,43 @@ with mp_hands.Hands(
       else: 
         fingersUp[0] = 0
 
-      distance1 = [lm[tipIds[0]].x*w - lm[tipIds[0]].y*h, lm[tipIds[2]].x*w - lm[tipIds[2]].y*h]
-      distance2 = [lm[tipIds[0]].x*w - lm[tipIds[0]].y*h, lm[tipIds[3]].x*w - lm[tipIds[3]].y*h]
-      if distance1[0] * distance1[1] > 0 and distance2[0] * distance2[1] > 0 and fingersUp[1] and fingersUp[4] and not fingersUp[2] and not fingersUp[3]:
-        if not konShow:
-          konShow = True
-          cv2.putText(image, "kon", (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
-          image = cv2.imread('Zorro_anime.png')
-          cv2.imshow('image window', image)
+      #Mostrar Kon
+      if fingersUp == [1,1,0,0,1] and not konShow:
+        konShow = True
+        #cv2.putText(image, "kon", (30,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
+        #kon = cv2.imread('Zorro_anime.png')
+        #cv2.imshow('kon', kon)        
+        #imgCanvas.fill(0)
       else:
         konShow = False
-        cv2.destroyAllWindows()
-        cv2.putText(image, str(sum(fingersUp)), (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)
+        kon = cv2.imread('Zorro_anime.png')
+        cv2.imshow('kon', kon)
+        cv2.destroyWindow('kon')
+      #Modo pintar
+      if fingersUp == [1,1,0,0,0] and not paintMode:
+        paintMode = True
+      elif fingersUp == [0,1,1,0,0] and paintMode:
+        paintMode = False
+      if fingersUp == [0,1,0,0,0] and paintMode:
+        cv2.circle(image, (int(lm[tipIds[1]].x*w),int(lm[tipIds[1]].y*h)), 10, (0,0,255), -1)
+        cv2.circle(imgCanvas, (int(lm[tipIds[1]].x*w),int(lm[tipIds[1]].y*h)), 10, (0,0,255), -1)
+      #Suma de dedos levantados
+      if paintMode:
+        cv2.putText(image, "Paint mode", (20,450),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0), thickness = 2)
+      else:
+        cv2.putText(image, "Paint mode", (20,450),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,225), thickness = 2)
+      cv2.putText(image, str(sum(fingersUp)), (20,60),cv2.FONT_HERSHEY_SIMPLEX,2,(255,0,0), thickness = 5)      
 
-    cv2.imshow('MediaPipe Hands', image)    
+    #Configuración del canvas
+    imgGray = cv2.cvtColor(imgCanvas, cv2.COLOR_BGR2GRAY)
+    _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
+    imgInv = cv2.cvtColor(imgInv, cv2.COLOR_GRAY2BGR)
+    image = cv2.bitwise_and(image, imgInv)
+    image = cv2.bitwise_or(image, imgCanvas)
+    
+    #Mostrar imágenes   
+    cv2.imshow('Canvas', imgCanvas) 
+    cv2.imshow('MediaPipe Hands', image)  
     
     if cv2.waitKey(5) & 0xFF == 27:
       break
